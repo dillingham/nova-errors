6c2f55fe3c35d69e4683669e6081c3cc7f4ce4b9
Brian Dillingham <bdillingham88@gmail.com>
2019-07-05 14:01:23 -0400

    Add modal ui

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,11 +1,23 @@
 <template>
-    <div v-if="errors.any()" class="p-6">
-        <div v-for="key in Object.keys(errors.all())" :key="key">
-            <div v-for="(error, index) in errors.get(key)" :key="index">
-                <span class="error-text mt-2 text-danger">{{ error }}</span>
+    <modal v-if="modalOpen" tabindex="-1" role="dialog" @modal-close="modalOpen = false">
+        <div class="bg-white rounded-lg shadow-lg overflow-hidden" style="width: 460px;">
+            <div class="p-8">
+                <h2 class="mb-6 text-90 font-normal text-xl">{{ __('Validation Errors') }}</h2>
+                <div v-for="key in Object.keys(errors.all())" :key="key">
+                    <div v-for="(error, index) in errors.get(key)" :key="index">
+                        <p class="error-text pb-1 text-danger">{{ error }}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-30 px-6 py-3 flex">
+                <div class="ml-auto">
+                    <button type="button" @click.prevent="modalOpen = false" class="btn text-80 font-normal h-9 px-3 mr-3 btn-link">
+                        {{ __('Close') }}
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
+    </modal>
 </template>
 
 <script>
@@ -20,10 +32,16 @@ export default {
         },
     },
 
+    data: function()  {
+        return {
+            modalOpen: false
+        }
+    },
+
     watch: {
         errors: function(errors) {
-            console.log(errors.all());
             window.scrollTo(0,0);
+            this.modalOpen = true;
         }
     }
 }
